8f13ccf959ed972e1b6e3da0c5dc8120e7fb4eb1
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-12-11 16:38:59 -0800

    Move `// @flow` to before license header (#3)
    Babel seems to not strip out Flow types (specifically generics) properly otherwise!

--- packages/regl-worldview/src/camera/index.js
+++ packages/regl-worldview/src/camera/index.js
@@ -1,10 +1,11 @@
+// @flow
+
 //  Copyright (c) 2018-present, GM Cruise LLC
 //
 //  This source code is licensed under the Apache License, Version 2.0,
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
 
-// @flow
 export { default as camera } from './camera';
 export { default as CameraListener } from './CameraListener';
 export { default as cameraProject } from './cameraProject';
--- packages/regl-worldview/src/stories/hitmap.js
+++ packages/regl-worldview/src/stories/hitmap.js
@@ -1,10 +1,10 @@
+// @flow
+
 //  Copyright (c) 2018-present, GM Cruise LLC
 //
 //  This source code is licensed under the Apache License, Version 2.0,
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
-
-// @flow
 
 import React from 'react';
 import styled from 'styled-components';

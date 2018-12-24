5cac13fed738efda34972306de073507cef471b7
Audrey Li <audrey.li@getcruise.com>
2018-12-24 12:12:12 -0800

    Address comments

--- docs/src/utils/GithubLink.js
+++ docs/src/utils/GithubLink.js
@@ -6,21 +6,32 @@
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
 
-import React from "react";
+import * as React from "react";
 import styled from "styled-components";
 
 import GithubLogo from "./GitHubLogo";
 
 const StyledLink = styled.a`
-  display: inline-block;
+  display: inline-flex;
+  align-items: center;
+  font-style: italic;
+`;
+const StyledChildrenWrapper = styled.span`
+  margin: 4px;
 `;
 
-type Props = {||};
+type Props = {|
+  path: string,
+  children: React.Node,
+  shouldPrefixLogo?: boolean,
+|};
 
-export default function GithubLink({ path, ...rest }: Props) {
+export default function GithubLink({ path, children, shouldPrefixLogo, ...rest }: Props) {
   return (
-    <StyledLink {...rest} title="Source Code" target="_blank" rel="noopener noreferrer">
-      <GithubLogo width={18} />
+    <StyledLink title="Source Code" target="_blank" rel="noopener noreferrer" {...rest}>
+      {shouldPrefixLogo && <GithubLogo width={16} />}
+      <StyledChildrenWrapper>{children} </StyledChildrenWrapper>
+      {!shouldPrefixLogo && <GithubLogo width={16} />}
     </StyledLink>
   );
 }

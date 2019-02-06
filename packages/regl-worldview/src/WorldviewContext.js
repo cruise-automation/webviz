74b7780bae697dc15e90e51aea4c184517d7d857
Robin Pham <robin.pham@gmail.com>
2019-02-06 15:58:46 -0800

    add guard

--- packages/regl-worldview/src/WorldviewContext.js
+++ packages/regl-worldview/src/WorldviewContext.js
@@ -273,7 +273,9 @@ export class WorldviewContext {
 
   callComponentHandlers = (objectId: number, ray: Ray, e: MouseEvent, mouseEventName: MouseEventEnum) => {
     this._hitmapCalls.forEach((_, component) => {
-      component.fireRelevantMouseHandler(objectId, e, ray, mouseEventName);
+      if (component.fireRelevantMouseHandler) {
+        component.fireRelevantMouseHandler(objectId, e, ray, mouseEventName);
+      }
     });
   };
 

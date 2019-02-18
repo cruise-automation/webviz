04e7c3466f5c1ae0437d0b1e24ce58f2b8c43fde
Troy Gibb <troy.gibb@getcruise.com>
2019-02-18 14:18:30 -0800

    Rename

--- packages/regl-worldview/src/commands/FilledPolygons.js
+++ packages/regl-worldview/src/commands/FilledPolygons.js
@@ -50,12 +50,12 @@ type Props = {
 export default function FilledPolygons({ children: polygons = [], getHitmapId, ...props }: Props) {
   const triangles = [];
   for (const poly of polygons) {
-    const { points, color, ...restOfProps } = poly;
+    const { points, color, ...restOfMarker } = poly;
     const pose = poly.pose ? poly.pose : NO_POSE;
     const earcutPoints: Vec3[] = getEarcutPoints(points);
     const polyPoints: Point[] = earcutPoints.map(([x, y, z]) => ({ x, y, z }));
     triangles.push({
-      ...restOfProps,
+      ...restOfMarker,
       points: polyPoints,
       pose,
       color: { r: color[0], g: color[1], b: color[2], a: color[3] },

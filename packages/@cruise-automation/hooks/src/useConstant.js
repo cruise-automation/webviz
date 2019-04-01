151621378a5bfd0967634ae238dd1d2ff7bb6d80
Audrey Li <audrey.li@getcruise.com>
2019-04-01 16:25:03 -0700

    Update useConstant

--- packages/@cruise-automation/hooks/src/useConstant.js
+++ packages/@cruise-automation/hooks/src/useConstant.js
@@ -6,13 +6,15 @@
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
 
-import { useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 // for sharing the same instance variable during the react life cycle
-export default function useConstant<T>(getValFn: () => T, teardown: (T) => void = () => {}): ?T {
-  const ref = useRef<T | null>(null);
-  ref.current = ref.current === null ? getValFn() : ref.current;
-  // To satisfy flow, we have to fallback to getValFn(), even though ref.current is not null after mount
-  useEffect(() => () => teardown(ref.current || getValFn()), []);
-  return ref.current || getValFn();
+export default function useConstant<T>(getValFn: () => T, teardown: (T) => any = () => {}): T {
+  const [value] = useState<T>(getValFn);
+  useEffect(() => {
+    return () => {
+      teardown(value);
+    };
+  }, []);
+  return value;
 }

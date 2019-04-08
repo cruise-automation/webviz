3e1e2b735df5bea50ec378c0436fb9d2d52923da
Audrey Li <audrey.li@getcruise.com>
2019-04-08 11:04:55 -0700

    Fix lint by removing 2 empty lines

--- packages/webviz-core/src/components/Panel.js
+++ packages/webviz-core/src/components/Panel.js
@@ -6,7 +6,6 @@
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
 
-
 import CloseIcon from "@mdi/svg/svg/close.svg";
 import FullscreenIcon from "@mdi/svg/svg/fullscreen.svg";
 import cx from "classnames";
--- packages/webviz-core/src/components/PlaybackControls/index.js
+++ packages/webviz-core/src/components/PlaybackControls/index.js
@@ -6,7 +6,6 @@
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
 
-
 import Tooltip from "@cruise-automation/tooltip";
 import CancelIcon from "@mdi/svg/svg/cancel.svg";
 import PauseIcon from "@mdi/svg/svg/pause.svg";

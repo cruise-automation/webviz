7464556b772aa296b7a041438d5294e6ca107ae9
Jacob Bandes-Storch <jacob@getcruise.com>
2019-11-27 15:51:07 -0800

    enable optional chaining (#277)

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = {
     "@babel/plugin-transform-modules-commonjs",
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-syntax-dynamic-import",
     ["@babel/plugin-proposal-object-rest-spread", { useBuiltIns: true }],
   ],

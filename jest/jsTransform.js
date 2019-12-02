edbfedf465f0f29ebfdafb12981b55f3c791deec
Jacob Bandes-Storch <jacob@getcruise.com>
2019-12-02 10:29:59 -0800

    fix jest config for CSS files imported from node_modules

--- jest/jsTransform.js
+++
@@ -1,8 +0,0 @@
-//  Copyright (c) 2018-present, GM Cruise LLC
-//
-//  This source code is licensed under the Apache License, Version 2.0,
-//  found in the LICENSE file in the root directory of this source tree.
-//  You may not use this file except in compliance with the License.
-
-// we use the same babel config from the .babelrc lookup path (package.json in our case)
-module.exports = require("babel-jest").createTransformer();

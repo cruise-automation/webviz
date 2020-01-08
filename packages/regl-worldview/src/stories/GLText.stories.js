963d8f20b331f27e4d9b8725b01d2ad9ddea9a2c
Troy Gibb <troy.gibb@getcruise.com>
2020-01-07 17:22:40 -0800

    Fix lint

--- packages/regl-worldview/src/stories/GLText.stories.js
+++ packages/regl-worldview/src/stories/GLText.stories.js
@@ -68,13 +68,13 @@ function hexToRgb(hex: string): Color {
     b = 0;
 
   // 3 digits
-  if (hex.length == 4) {
+  if (hex.length === 4) {
     r = `0x${hex[1]}${hex[1]}`;
     g = `0x${hex[2]}${hex[2]}`;
     b = `0x${hex[3]}${hex[3]}`;
 
     // 6 digits
-  } else if (hex.length == 7) {
+  } else if (hex.length === 7) {
     r = `0x${hex[1]}${hex[2]}`;
     g = `0x${hex[3]}${hex[4]}`;
     b = `0x${hex[5]}${hex[6]}`;

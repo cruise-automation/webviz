ef1b8a4d28fefd34e3bfeadc24931590677388b6
Jacob Bandes-Storch <jacob@getcruise.com>
2020-01-07 22:31:05 -0800

    add test fixture

--- packages/webviz-core/src/dataProviders/BagDataProvider.test.js
+++ packages/webviz-core/src/dataProviders/BagDataProvider.test.js
@@ -49,6 +49,40 @@ describe("BagDataProvider", () => {
     ]);
   });
 
+  it("initializes with bz2 bag", async () => {
+    const provider = new BagDataProvider(
+      { bagPath: { type: "file", file: `${__dirname}/../../public/fixtures/example-bz2.bag` } },
+      []
+    );
+    const result = await provider.initialize(dummyExtensionPoint);
+    expect(result.start).toEqual({ sec: 1396293887, nsec: 844783943 });
+    expect(result.end).toEqual({ sec: 1396293909, nsec: 544870199 });
+    expect(result.topics).toContainOnly([
+      { datatype: "rosgraph_msgs/Log", name: "/rosout" },
+      { datatype: "turtlesim/Color", name: "/turtle1/color_sensor" },
+      { datatype: "tf2_msgs/TFMessage", name: "/tf_static" },
+      { datatype: "turtlesim/Color", name: "/turtle2/color_sensor" },
+      { datatype: "turtlesim/Pose", name: "/turtle1/pose" },
+      { datatype: "turtlesim/Pose", name: "/turtle2/pose" },
+      { datatype: "tf/tfMessage", name: "/tf" },
+      { datatype: "geometry_msgs/Twist", name: "/turtle2/cmd_vel" },
+      { datatype: "geometry_msgs/Twist", name: "/turtle1/cmd_vel" },
+    ]);
+    expect(Object.keys(result.datatypes)).toContainOnly([
+      "rosgraph_msgs/Log",
+      "std_msgs/Header",
+      "turtlesim/Color",
+      "tf2_msgs/TFMessage",
+      "geometry_msgs/TransformStamped",
+      "geometry_msgs/Transform",
+      "geometry_msgs/Vector3",
+      "geometry_msgs/Quaternion",
+      "turtlesim/Pose",
+      "tf/tfMessage",
+      "geometry_msgs/Twist",
+    ]);
+  });
+
   it("calls progress callback while initializing with a local bag", async () => {
     const provider = new BagDataProvider(
       { bagPath: { type: "file", file: `${__dirname}/../../public/fixtures/example.bag` } },

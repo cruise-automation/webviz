e017cc1cfccb6a063b6a01ac4f4fb54ba83105d2
Troy Gibb <troy.gibb@getcruise.com>
2020-01-07 16:19:59 -0800

    Add custom highlight color prop

--- packages/regl-worldview/src/stories/GLText.stories.js
+++ packages/regl-worldview/src/stories/GLText.stories.js
@@ -7,6 +7,7 @@ import React, { useState, useLayoutEffect } from "react";
 import { withScreenshot } from "storybook-chrome-screenshot";
 
 import { Axes } from "../commands";
+import type { Color } from "../types";
 import { vec4ToOrientation } from "../utils/commandUtils";
 import Container from "./Container";
 import inScreenshotTests from "stories/inScreenshotTests";
@@ -39,6 +40,50 @@ function textMarkers({
       billboard,
     };
   });
+}
+
+function rgbToHex(color: Color) {
+  let r = Math.floor(color.r * 255).toString(16),
+    g = Math.floor(color.g * 255).toString(16),
+    b = Math.floor(color.b * 255).toString(16);
+
+  if (r.length === 1) {
+    r = `0${r}`;
+  }
+  if (g.length === 1) {
+    g = `0${g}`;
+  }
+  if (b.length === 1) {
+    b = `0${b}`;
+  }
+
+  return `#${r}${g}${b}`;
+}
+
+function hexToRgb(hex: string): Color {
+  let r = 0,
+    g = 0,
+    b = 0;
+
+  // 3 digits
+  if (hex.length == 4) {
+    r = `0x${hex[1]}${hex[1]}`;
+    g = `0x${hex[2]}${hex[2]}`;
+    b = `0x${hex[3]}${hex[3]}`;
+
+    // 6 digits
+  } else if (hex.length == 7) {
+    r = `0x${hex[1]}${hex[2]}`;
+    g = `0x${hex[3]}${hex[4]}`;
+    b = `0x${hex[5]}${hex[6]}`;
+  }
+
+  // TIL you can do hexadecimal math with JS strings. WTF. This has
+  // to have led to the craziest bug hunts ever conceived.
+  r = +(r / 255).toFixed(2);
+  g = +(g / 255).toFixed(2);
+  b = +(b / 255).toFixed(2);
+  return { r, g, b, a: 1 };
 }
 
 storiesOf("Worldview/GLText", module)
@@ -92,6 +137,7 @@ storiesOf("Worldview/GLText", module)
   .add("highlighted text", () => {
     const Example = () => {
       const [searchText, setSearchText] = useState("ello\nW");
+      const [highlightColor, setHighlightColor] = useState<Color>({ r: 1, b: 0, g: 1, a: 1 });
       const markers = textMarkers({ text: "Hello\nWorldview" }).map((marker) => {
         if (!searchText) {
           return marker;
@@ -111,26 +157,39 @@ storiesOf("Worldview/GLText", module)
             highlightedIndices.add(match.index + i);
           });
         }
-        return { ...marker, highlightedIndices: Array.from(highlightedIndices) };
+        return { ...marker, highlightedIndices: Array.from(highlightedIndices), highlightColor };
       });
 
       return (
-        <>
-          <div style={{ width: "100%", height: "95%" }}>
+        <div style={{ width: "100%", height: "100%" }}>
+          <div style={{ width: "100%", height: "100%" }}>
             <Container cameraState={{ perspective: true, distance: 40 }} backgroundColor={[0.2, 0.2, 0.4, 1]}>
               <GLText autoBackgroundColor>{markers}</GLText>
               <Axes />
             </Container>
           </div>
-
-          <input
-            style={{ width: "100%" }}
-            type="text"
-            placeholder="search text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-        </>
+          <div style={{ position: "absolute", top: "10px", right: "10px" }}>
+            <label htmlFor="search">Search: </label>
+            <input
+              type="text"
+              name="search"
+              placeholder="search text"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <label htmlFor="highlight-color">Highlight Color: </label>
+            <input
+              type="color"
+              name="highlight-color"
+              value={rgbToHex(highlightColor)}
+              onChange={(e) => {
+                const hex = e.target.value;
+                const newColor = hexToRgb(hex);
+                setHighlightColor({ ...newColor, a: 1 });
+              }}
+            />
+          </div>
+        </div>
       );
     };
 

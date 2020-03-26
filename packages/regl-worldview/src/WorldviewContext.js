349859802559d82adaad58720ba0ef1316bcbf0f
Josh Ribakoff <josh.ribakoff@getcruise.com>
2020-03-26 15:35:02 -0700

    lowercase number

--- packages/regl-worldview/src/WorldviewContext.js
+++ packages/regl-worldview/src/WorldviewContext.js
@@ -79,7 +79,7 @@ function compile<T>(regl: any, cmd: RawCommand<T>): CompiledReglCommand<T> {
 // draw calls, hitmap calls, and raycasting.
 
 export class WorldviewContext {
-  _lastPaintCall: Number;
+  _lastPaintCall: number;
   _commands: Set<RawCommand<any>> = new Set();
   _compiled: Map<Function, CompiledReglCommand<any>> = new Map();
   _drawCalls: Map<React.Component<any>, DrawInput> = new Map();

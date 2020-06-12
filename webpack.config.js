89516ab18bb795f0c2a838214437da072603b470
David Winegar <david.winegar@getcruise.com>
2020-06-12 13:22:35 -0700

    Fix pregenerated atlas
    Make the internal repo able to use this correctly with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -159,7 +159,7 @@ module.exports = {
       },
       { test: /\.scss$/, loader: "sass-loader", options: { sourceMap: true } },
       { test: /\.woff2?$/, loader: "url-loader" },
-      { test: /\.(glb|bag|ttf)$/, loader: "file-loader" },
+      { test: /\.(glb|bag|ttf|bin)$/, loader: "file-loader" },
       {
         test: /node_modules\/compressjs\/.*\.js/,
         loader: "string-replace-loader",

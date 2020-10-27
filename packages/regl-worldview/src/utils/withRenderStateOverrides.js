fa634add7c731e712bda1f1310c54fa04407e21e
Hernan Saez <hhsaez@gmail.com>
2020-10-27 15:35:37 -0300

    Worldview: Fix render state memoization to avoid performance issues (#536)
    * Fix memoization to avoid performance issues
    
    * Update packages/regl-worldview/src/utils/withRenderStateOverrides.js
    
    Co-authored-by: Chris Hasson <chris@remix.com>
    
    Co-authored-by: Chris Hasson <chris@remix.com>

--- packages/regl-worldview/src/utils/withRenderStateOverrides.js
+++ packages/regl-worldview/src/utils/withRenderStateOverrides.js
@@ -15,21 +15,22 @@ const withRenderStateOverrides = (command: any) => (regl: any) => {
   // Generate the render command once
   const reglCommand = command(regl);
 
+  // Use memoization to avoid generating multiple render commands for the same render states
+  // for the same render states
+  const memoizedRender = memoize(
+    (props: { depth: DepthState, blend: BlendState }) => {
+      const { depth, blend } = props;
+      return regl({ ...reglCommand, depth, blend });
+    },
+    (...args) => JSON.stringify(args)
+  );
+
   const renderElement = (props) => {
     // Get curstom render states from the given marker. Some commands, like <Arrows />
     // will use the originalMarker property instead. If no custom render states
     // are present, use the default ones to make sure the hitmap works correctly.
     const depth = props.depth || props.originalMarker?.depth || defaultReglDepth;
     const blend = props.blend || props.originalMarker?.blend || defaultReglBlend;
-
-    // Use memoization to prevent generating too multiple render commands
-    // for the same render states
-    const memoizedRender = memoize(
-      (props: { depth: DepthState, blend: BlendState }) => {
-        return regl({ ...reglCommand, depth, blend });
-      },
-      (...args) => JSON.stringify(args)
-    );
     memoizedRender({ depth, blend })(props);
   };
 

8806e58906743d21a77dcdc560b17394c4cce7b7
jreyesr <jreyesr@users.noreply.github.com>
2020-12-16 20:19:03 -0500

    Change to functional (?) component
    Remove color setting for buttons
    Add help popup

--- packages/webviz-core/src/panels/Teleop/index.js
+++ packages/webviz-core/src/panels/Teleop/index.js
@@ -5,9 +5,10 @@
 //  This source code is licensed under the Apache License, Version 2.0,
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
-import * as React from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import { hot } from "react-hot-loader/root";
 
+import helpContent from "./index.help.md";
 import { PanelToolbarInput, PanelToolbarLabel } from "webviz-core/shared/panelToolbarStyles";
 import Button from "webviz-core/src/components/Button";
 import Flex from "webviz-core/src/components/Flex";
@@ -34,70 +35,15 @@ type Props = {
   datatypes: RosDatatypes,
 };
 
-type PanelState = {|
-  cachedProps: $Shape<Props>,
-  datatypeNames: string[],
-  parsedObject: ?any,
-  error: ?string,
-  pressing: any,
-  topic: string,
-|};
+function Teleop(props: Props) {
+  const { config, saveConfig, capabilities } = props;
+  const { topicName } = config;
 
-function parseInput(value: string): $Shape<PanelState> {
-  let parsedObject;
-  let error = null;
-  try {
-    const parsedAny = JSON.parse(value);
-    if (Array.isArray(parsedAny)) {
-      error = "Message content must be an object, not an array";
-    } else if (parsedAny === null) {
-      error = "Message content must be an object, not null";
-    } else if (typeof parsedAny !== "object") {
-      error = `Message content must be an object, not ‘${typeof parsedAny}’`;
-    } else {
-      parsedObject = parsedAny;
-    }
-  } catch (e) {
-    error = value ? e.message : "";
-  }
-  return { error, parsedObject };
-}
+  const publisher = React.createRef<Publisher>();
 
-class Teleop extends React.PureComponent<Props, PanelState> {
-  static panelType = "Teleop";
-  static defaultConfig = {
-    buttonColor: "#00A871",
-    topicName: "/cmd_vel",
-  };
+  const [pressing, setPressing] = useState({ ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false });
 
-  _publisher = React.createRef<Publisher>();
-
-  state = {
-    cachedProps: {},
-    datatypeNames: [],
-    error: null,
-    parsedObject: undefined,
-    pressing: { ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false },
-  };
-
-  static getDerivedStateFromProps(props: Props, state: PanelState) {
-    const newState: $Shape<PanelState> = parseInput(props.config.value);
-    let changed = false;
-
-    if (props !== state.cachedProps) {
-      newState.cachedProps = props;
-      changed = true;
-    }
-
-    if (props.datatypes !== state.cachedProps.datatypes) {
-      newState.datatypeNames = Object.keys(props.datatypes).sort();
-      changed = true;
-    }
-
-    return changed ? newState : null;
-  }
-
-  composeTwist = (key) => {
+  const composeTwist = (key) => {
     const COMMANDS = {
       // Map x, y, z, th here
       ArrowUp: [1, 0, 0, 0],
@@ -123,47 +69,45 @@ class Teleop extends React.PureComponent<Props, PanelState> {
     };
   };
 
-  _onChange = (event: SyntheticInputEvent<HTMLTextAreaElement>) => {
-    this.props.saveConfig({ value: event.target.value });
-  };
+  // https://reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
-  _onCommandButtonClick = (command) => {
-    // Simulate key press and release
-    this._handleKey({ key: command, type: "keydown" });
-    this._handleKey({ key: command, type: "keyup" });
-  };
-
-  _handleKey = (event) => {
-    this.setState(function(state, _) {
-      const newPressing = state.pressing;
-      newPressing[event.key] = event.type === "keydown";
-      if (this._publisher.current) {
-        if (event.type === "keydown") {
-          this._publisher.current.publish(this.composeTwist(event.key));
-        }
-      } else {
-        console.error("Publisher not set!");
+  const handleKey = (event) => {
+    const newPressing = pressing;
+    newPressing[event.key] = event.type === "keydown";
+    if (publisher.current) {
+      if (event.type === "keydown") {
+        publisher.current.publish(composeTwist(event.key));
       }
-      return {
-        pressing: newPressing,
-      };
-    });
+    } else {
+      console.error("Publisher not set!");
+    }
+    setPressing(newPressing);
 
-    this.forceUpdate(); // HACK Appears to be required to update the colors quickly???
+    // HACK Appears to be required to update the colors quickly (to give feedback when using keyboard)
+    forceUpdate();
   };
 
   // Add handler here for all desired keybindings (they are all the same!)
-  _keyHandlers = {
-    ArrowLeft: this._handleKey,
-    ArrowRight: this._handleKey,
-    ArrowUp: this._handleKey,
-    ArrowDown: this._handleKey,
-    " ": this._handleKey, // Space
+  const keyHandlers = {
+    ArrowLeft: handleKey,
+    ArrowRight: handleKey,
+    ArrowUp: handleKey,
+    ArrowDown: handleKey,
+    " ": handleKey, // Space
   };
 
-  _renderMenuContent() {
-    const { config, saveConfig } = this.props;
+  const onCommandButtonClick = (command) => {
+    // Simulate key press and release
+    handleKey({ key: command, type: "keydown" });
+    handleKey({ key: command, type: "keyup" });
+  };
 
+  const buttonColor = "#00A871";
+
+  const canPublish = capabilities.includes(PlayerCapabilities.advertise) && topicName;
+
+  const renderMenuContent = () => {
     return (
       <>
         <Item>
@@ -179,70 +123,62 @@ class Teleop extends React.PureComponent<Props, PanelState> {
         </Item>
       </>
     );
-  }
+  };
 
-  render() {
-    const {
-      capabilities,
-      config: { topicName },
-    } = this.props;
-    const buttonColor = "#00A871";
+  return (
+    <Flex col style={{ height: "100%", padding: "12px" }}>
+      {topicName && <Publisher ref={publisher} name="Publish" topic={topicName} datatype="geometry_msgs/Twist" />}
 
-    const { pressing } = this.state;
-    const canPublish = capabilities.includes(PlayerCapabilities.advertise);
+      <KeyListener keyDownHandlers={keyHandlers} keyUpHandlers={keyHandlers} />
 
-    return (
-      <Flex col style={{ height: "100%", padding: "12px" }} onKeyPress={this.handleKey}>
-        {topicName && (
-          <Publisher ref={this._publisher} name="Publish" topic={topicName} datatype="geometry_msgs/Twist" />
-        )}
-
-        <KeyListener keyDownHandlers={this._keyHandlers} keyUpHandlers={this._keyHandlers} />
-        <PanelToolbar floating menuContent={this._renderMenuContent()} />
-        <Flex row style={{ flex: "0 0 auto", justifyContent: "center", paddingTop: "5px", paddingBottom: "5px" }}>
-          <Button
-            style={{ backgroundColor: pressing.ArrowUp ? colors.red : buttonColor }}
-            disabled={!canPublish}
-            tooltip={canPublish ? "" : "Connect to ROS to publish data"}
-            onClick={(_) => this._onCommandButtonClick("ArrowUp")}>
-            ↑
-          </Button>
-        </Flex>
-        <Flex row style={{ flex: "0 0 auto", justifyContent: "center", paddingTop: "5px", paddingBottom: "5px" }}>
-          <Button
-            style={{ backgroundColor: pressing.ArrowLeft ? colors.red : buttonColor }}
-            disabled={!canPublish}
-            tooltip={canPublish ? "" : "Connect to ROS to publish data"}
-            onClick={(_) => this._onCommandButtonClick("ArrowLeft")}>
-            ←
-          </Button>
-          <Button
-            style={{ backgroundColor: pressing.ArrowDown ? colors.red : buttonColor }}
-            disabled={!canPublish}
-            tooltip={canPublish ? "" : "Connect to ROS to publish data"}
-            onClick={(_) => this._onCommandButtonClick("ArrowDown")}>
-            ↓
-          </Button>
-          <Button
-            style={{ backgroundColor: pressing.ArrowRight ? colors.red : buttonColor }}
-            disabled={!canPublish}
-            tooltip={canPublish ? "" : "Connect to ROS to publish data"}
-            onClick={(_) => this._onCommandButtonClick("ArrowRight")}>
-            →
-          </Button>
-        </Flex>
-        <Flex row style={{ flex: "0 0 auto", justifyContent: "center", paddingTop: "5px", paddingBottom: "5px" }}>
-          <Button
-            style={{ backgroundColor: pressing[" "] ? buttonColor : colors.red }}
-            disabled={!canPublish}
-            tooltip={canPublish ? "" : "Connect to ROS to publish data"}
-            onClick={(_) => this._onCommandButtonClick(" ")}>
-            Stop
-          </Button>
-        </Flex>
+      <PanelToolbar floating menuContent={renderMenuContent()} helpContent={helpContent} />
+      <Flex row style={{ flex: "0 0 auto", justifyContent: "center", paddingTop: "5px", paddingBottom: "5px" }}>
+        <Button
+          style={{ backgroundColor: pressing.ArrowUp ? colors.red : buttonColor }}
+          disabled={!canPublish}
+          tooltip={canPublish ? "" : "Connect to ROS to publish data"}
+          onClick={(_) => onCommandButtonClick("ArrowUp")}>
+          ↑
+        </Button>
       </Flex>
-    );
-  }
+      <Flex row style={{ flex: "0 0 auto", justifyContent: "center", paddingTop: "5px", paddingBottom: "5px" }}>
+        <Button
+          style={{ backgroundColor: pressing.ArrowLeft ? colors.red : buttonColor }}
+          disabled={!canPublish}
+          tooltip={canPublish ? "" : "Connect to ROS to publish data"}
+          onClick={(_) => onCommandButtonClick("ArrowLeft")}>
+          ←
+        </Button>
+        <Button
+          style={{ backgroundColor: pressing.ArrowDown ? colors.red : buttonColor }}
+          disabled={!canPublish}
+          tooltip={canPublish ? "" : "Connect to ROS to publish data"}
+          onClick={(_) => onCommandButtonClick("ArrowDown")}>
+          ↓
+        </Button>
+        <Button
+          style={{ backgroundColor: pressing.ArrowRight ? colors.red : buttonColor }}
+          disabled={!canPublish}
+          tooltip={canPublish ? "" : "Connect to ROS to publish data"}
+          onClick={(_) => onCommandButtonClick("ArrowRight")}>
+          →
+        </Button>
+      </Flex>
+      <Flex row style={{ flex: "0 0 auto", justifyContent: "center", paddingTop: "5px", paddingBottom: "5px" }}>
+        <Button
+          style={{ backgroundColor: pressing[" "] ? buttonColor : colors.red }}
+          disabled={!canPublish}
+          tooltip={canPublish ? "" : "Connect to ROS to publish data"}
+          onClick={(_) => onCommandButtonClick(" ")}>
+          Stop
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
 
+Teleop.panelType = "Teleop";
+Teleop.defaultConfig = {
+  topicName: "/cmd_vel",
+};
 export default hot(Panel<Config>(Teleop));

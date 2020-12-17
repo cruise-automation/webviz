5575c2252d9f0cd9d1810b6ff197108891734de8
jreyesr <jreyesr@users.noreply.github.com>
2020-12-16 20:56:06 -0500

    Fix linter error

--- packages/webviz-core/src/panels/Teleop/index.js
+++ packages/webviz-core/src/panels/Teleop/index.js
@@ -5,7 +5,7 @@
 //  This source code is licensed under the Apache License, Version 2.0,
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useState, useReducer } from "react";
 import { hot } from "react-hot-loader/root";
 
 import helpContent from "./index.help.md";

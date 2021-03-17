63f0905e885c9bc09bca59bd255f7e83d3963684
Hernan Saez <hhsaez@gmail.com>
2021-03-17 15:17:45 -0300

    Add story for model with no default samplers

--- packages/regl-worldview/src/stories/GLTFScene.stories.js
+++ packages/regl-worldview/src/stories/GLTFScene.stories.js
@@ -8,26 +8,49 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 
 import Worldview, { Axes, GLTFScene, Grid } from "../index";
-import duckModel from "common/fixtures/Duck.glb"; // Webpack magic: we actually import a URL pointing to a .glb file
 
-storiesOf("Worldview/GLTFScene", module).add("<GLTFScene> - Load a scene from file", () => {
-  return (
-    <Worldview
-      defaultCameraState={{
-        distance: 25,
-        thetaOffset: (-3 * Math.PI) / 4,
-      }}>
-      <Axes />
-      <Grid />
-      <GLTFScene model={duckModel}>
-        {{
-          pose: {
-            position: { x: 0, y: 3, z: 0 },
-            orientation: { x: 0, y: 0, z: 1, w: 0 },
-          },
-          scale: { x: 1, y: 1, z: 1 },
-        }}
-      </GLTFScene>
-    </Worldview>
-  );
-});
+storiesOf("Worldview/GLTFScene", module)
+  .add("<GLTFScene> - Load a scene from file", () => {
+    const model = require("common/fixtures/Duck.glb");
+    return (
+      <Worldview
+        defaultCameraState={{
+          distance: 25,
+          thetaOffset: (-3 * Math.PI) / 4,
+        }}>
+        <Axes />
+        <Grid />
+        <GLTFScene model={model}>
+          {{
+            pose: {
+              position: { x: 0, y: 3, z: 0 },
+              orientation: { x: 0, y: 0, z: 1, w: 0 },
+            },
+            scale: { x: 1, y: 1, z: 1 },
+          }}
+        </GLTFScene>
+      </Worldview>
+    );
+  })
+  .add("<GLTFScene> - Load a scene with default texture samplers", () => {
+    const model = require("common/fixtures/gltf_bug_sampler.glb");
+    return (
+      <Worldview
+        defaultCameraState={{
+          distance: 25,
+          thetaOffset: (-3 * Math.PI) / 4,
+        }}>
+        <Axes />
+        <Grid />
+        <GLTFScene model={model}>
+          {{
+            pose: {
+              position: { x: 0, y: 3, z: 0 },
+              orientation: { x: 0, y: 0, z: 1, w: 0 },
+            },
+            scale: { x: 0.01, y: 0.01, z: 0.01 },
+          }}
+        </GLTFScene>
+      </Worldview>
+    );
+  });

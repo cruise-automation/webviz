c16ea8fe1428e1b9ded1da8d2cba9645371f3530
Dmitry Panin <pankdm@gmail.com>
2022-02-18 10:20:59 -0800

    chown

--- run_ci.sh
+++ run_ci.sh
@@ -1,14 +1,15 @@
 # npm run ci
 set -e
 
+chown -R root:root .
+
 npm run whoami
 whoami
-echo "installing ffmpeg now"
 
 # export npm_config_cache=./npm_cache
-npm run install-ffmpeg
+# npm run install-ffmpeg
 # integration-tests/install-ffmpeg-in-docker.sh
 
 # npm ci
 # npm run bootstrap-lerna
-# npm run ci
+npm run ci

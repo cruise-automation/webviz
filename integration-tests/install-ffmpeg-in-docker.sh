7782eadc00f98727bac9e1ea22651d5b0d65d556
Dmitry Panin <pankdm@gmail.com>
2022-02-17 21:50:37 -0800

    Better config

--- integration-tests/install-ffmpeg-in-docker.sh
+++ integration-tests/install-ffmpeg-in-docker.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 sed -i "s/jessie main/jessie main contrib non-free/" /etc/apt/sources.list
 echo "deb [check-valid-until=no] http://archive.debian.org/debian jessie-backports main contrib non-free" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8B48AD6246925553 7638D0442B90D010
 apt-get update && apt-get install -y ffmpeg
--- run_ci.sh
+++ run_ci.sh
@@ -1,7 +1,9 @@
 # npm run ci
 set -e
 
-export npm_config_cache=./npm_cache
+# export npm_config_cache=./npm_cache
+
+integration-tests/install-ffmpeg-in-docker.sh
 
 # npm ci
 # npm run bootstrap-lerna

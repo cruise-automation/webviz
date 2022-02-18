4cd3d8ea6f0e70f3a8689fbe768b759e357464d7
Dmitry Panin <pankdm@gmail.com>
2022-02-18 09:45:18 -0800

    simplifying failure to only install

--- run_ci.sh
+++ run_ci.sh
@@ -1,10 +1,14 @@
 # npm run ci
 set -e
 
-# export npm_config_cache=./npm_cache
+npm run whoami
+whoami
+echo "installing ffmpeg now"
 
-integration-tests/install-ffmpeg-in-docker.sh
+# export npm_config_cache=./npm_cache
+npm run install-ffmpeg
+# integration-tests/install-ffmpeg-in-docker.sh
 
 # npm ci
 # npm run bootstrap-lerna
-npm run ci
+# npm run ci

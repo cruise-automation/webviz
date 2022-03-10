d13afdacaec0b48f983adcaca55b84c36c42a5f2
Dmitry Panin <pankdm@gmail.com>
2022-03-10 12:23:03 -0800

    Fix repository update (#734)

--- integration-tests/install-ffmpeg-in-docker.sh
+++ integration-tests/install-ffmpeg-in-docker.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Install ffmpeg. Validity check disabled because the repository is no longer updated.
+# [check-valid-until=no]: https://unix.stackexchange.com/a/508728
+# [trusted=yes]: https://unix.stackexchange.com/a/602746
 sed -i "s/jessie main/jessie main contrib non-free/" /etc/apt/sources.list
-echo "deb [check-valid-until=no] http://archive.debian.org/debian jessie-backports main contrib non-free" >> /etc/apt/sources.list
+echo "deb [check-valid-until=no, trusted=yes] http://archive.debian.org/debian jessie-backports main contrib non-free" >> /etc/apt/sources.list
 # Adding the keys to fix the error:
 # ```
 # #8 2.073 W: GPG error: http://archive.debian.org/debian jessie-backports InRelease:
